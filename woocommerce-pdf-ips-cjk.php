d1b1b848c0a9f97ad4936729e74d3125404e9492
Alexandre Faustino <alexmigf@gmail.com>
2024-12-30 09:58:41 +0000

    First commit

---
+++ woocommerce-pdf-ips-cjk.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:      PDF Invoices & Packing Slips for WooCommerce - mPDF CJK
+ * Requires Plugins: woocommerce-pdf-ips-mpdf
+ * Plugin URI:       https://github.com/wpovernight/woocommerce-pdf-ips-mpdf-cjk
+ * Description:      Chinese, Japanese, and Korean font pack for the mPDF extension.
+ * Version:          1.0.0
+ * Author:           WP Overnight
+ * Author URI:       https://wpovernight.com
+ * License:          GPLv3
+ * License URI:      https://opensource.org/licenses/gpl-license.php
+ * Text Domain:      woocommerce-pdf-ips-mpdf-cjk
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+function wpo_ips_mpdf_cjk_load_fonts_dir( $mpdf ) {
+	if ( ! $mpdf instanceof \Mpdf\Mpdf || ! method_exists( $mpdf, 'AddFontDirectory' ) ) {
+		wcpdf_log_error( 'wpo_ips_mpdf_cjk_load_fonts_dir: mPDF not found or AddFontDirectory method not available' );
+		return $mpdf;
+	}
+	
+	$mpdf->AddFontDirectory( __DIR__ . '/fonts' );
+
+	return $mpdf;
+}
+
+add_filter( 'wpo_wcpdf_before_mpdf_write', 'wpo_ips_mpdf_cjk_load_fonts_dir', 10, 1 );
